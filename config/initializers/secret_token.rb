6a035b090093cd4786a1fea2a678695c538b6341
Mateusz Król <kroolikk@gmail.com>
2012-08-01 22:53:51 +0200

    created new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlewDev1::Application.config.secret_token = 'cdec0158379efecdaa4279ab420cbecb923106be69c71be0fd70df049ccda74083c7ce3d35427d6cd229ac4336461c08d2a6c4efdfc8ddda35ade81ed0970cc2'
